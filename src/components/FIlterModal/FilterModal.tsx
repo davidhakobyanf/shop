ce57044bdd741650b88f3bba8a21ba73d55edb37
davidhakobyanf <hakobyan.david2003@gmail.com>
2025-02-20 14:27:11 +0400

    sec

--- src/components/FIlterModal/FilterModal.tsx
+++ src/components/FIlterModal/FilterModal.tsx
@@ -1,5 +1,6 @@
-import styles from './FilterModal.module.css';
+import React, { useEffect, useState } from 'react';
 import useOutSideClick from '../hook/useOutSideClick';
+import styles from './FilterModal.module.css';
 
 export interface FilterModalProps {
     children: React.ReactNode;
@@ -8,10 +9,37 @@ export interface FilterModalProps {
 
 const FilterModal: React.FC<FilterModalProps> = ({ children, label }) => {
     const { isOpenOutSide, open, close, dropDownRef } = useOutSideClick();
+    const [modalStyle, setModalStyle] = useState({});
+
+    const calculateModalPosition = () => {
+        if (dropDownRef.current) {
+            const modalRect = dropDownRef.current.getBoundingClientRect();
+            const viewportWidth = window.innerWidth;
+
+            if (modalRect.right > viewportWidth) {
+                setModalStyle({ right: '0px', left: 'auto' });
+            } else {
+                setModalStyle({ left: '0px', right: 'auto' });
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (isOpenOutSide) {
+            calculateModalPosition();
+        } else {
+            setModalStyle({});
+        }
+    }, [isOpenOutSide]);
+
     return (
         <div className={`${styles.filterContainer} ${styles.filterActive}`} onMouseEnter={open} onMouseLeave={close}>
             <span>{label}</span>
-            <div ref={dropDownRef} className={styles.modal} style={{ display: isOpenOutSide ? 'flex' : 'none' }}>
+            <div
+                ref={dropDownRef}
+                className={styles.modal}
+                style={{ display: isOpenOutSide ? 'flex' : 'none', ...modalStyle }}
+            >
                 <div className={styles.buffer} />
                 {children}
             </div>
